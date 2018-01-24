eac7cbe3d870f596fc37af183dceb96df63a149a
Harpreet Singh <harry.singh1064@gmail.com>
2018-01-24 13:08:02 +0530

    Add checking storeId in req.params incase not present in req.body

--- Middlewares/storeCheck.js
+++ Middlewares/storeCheck.js
@@ -8,7 +8,7 @@ let HandleApiError = require('../Utility/HandleApiError');
 module.exports = async function (req, res, next) {
     try {
         let body = req.body;
-        let storeId = body.storeId;
+        let storeId = body.storeId ? body.storeId: req.params.storeId;
         let store = await StoreModel.getStore(storeId);
         if (!store) {
             res.json({
@@ -16,6 +16,7 @@ module.exports = async function (req, res, next) {
                 message: `Store with id ${storeId} does not exist.`
             });
         } else {
+            req.store = store;
             next()
         }
     } catch (e) {
